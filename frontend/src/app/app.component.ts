3ae0b07b3f37433942a5c19f4d6e659a304d0f9e
mazoorek <piotr.mazurek98@gmail.com>
2019-11-17 12:10:30 +0100

    pierwszy refactor navbaru( i przy okazji styli)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,67 +8,57 @@ export interface PageSection {
 @Component({
   selector: 'app-root',
   template: `
-    <section class="et-hero-tabs" #header>
-      <h1>STICKY SLIDER NAV</h1>
-      <h3>Sliding content with sticky tab nav</h3>
-      <div class="et-hero-tabs-container" [ngClass]="{'et-hero-tabs-container--top': fixed}">
-        <a class="et-hero-tab" #HomeNavbarItem
-           (click)="navigationItemClicked(home, homeNavbarItem)">Home</a>
-        <a class="et-hero-tab" #OddziałyNavbarItem
-           (click)="navigationItemClicked(oddzialy, oddzialyNavbarItem)">Oddziały</a>
-        <a class="et-hero-tab" #PacjenciNavbarItem
-           (click)="navigationItemClicked(pacjenci, pacjenciNavbarItem)">Pacjenci</a>
-        <a class="et-hero-tab" #PracownicyNavbarItem
-           (click)="navigationItemClicked(pracownicy, pracownicyNavbarItem)">Pracownicy</a>
-        <a class="et-hero-tab" #LekarzeNavbarItem
-           (click)="navigationItemClicked(lekarze, lekarzeNavbarItem)">Lekarze</a>
-        <a class="et-hero-tab" #PersonelNavbarItem
-           (click)="navigationItemClicked(personel, personelNavbarItem)">Personel</a>
-        <a class="et-hero-tab" #UrlopyNavbarItem
-           (click)="navigationItemClicked(urlopy, urlopyNavbarItem)">Urlopy</a>
-        <a class="et-hero-tab" #PokojeNavbarItem
-           (click)="navigationItemClicked(pokoje, pokojeNavbarItem)">Pokoje</a>
-        <span class="et-hero-tab-slider" [ngStyle]="{'width.px': sliderWidth, 'left.px': sliderLeft}"></span>
+      <div class="header-section" #header>
+          <h1 class="title">HOSPITAL MANAGEMENT</h1>
+          <h3 class="subtitle">Aplikacja do zarządzania szpitalem</h3>
+          <div class="navbar-container" [ngClass]="{'fixed': fixed}">
+              <a class="navbar-item" #HomeNavbarItem
+                 (click)="navigationItemClicked(home, homeNavbarItem)">Home</a>
+              <a class="navbar-item" #OddzialyNavbarItem
+                 (click)="navigationItemClicked(oddzialy, oddzialyNavbarItem)">Oddziały</a>
+              <a class="navbar-item" #PacjenciNavbarItem
+                 (click)="navigationItemClicked(pacjenci, pacjenciNavbarItem)">Pacjenci</a>
+              <a class="navbar-item" #PracownicyNavbarItem
+                 (click)="navigationItemClicked(pracownicy, pracownicyNavbarItem)">Pracownicy</a>
+              <a class="navbar-item" #LekarzeNavbarItem
+                 (click)="navigationItemClicked(lekarze, lekarzeNavbarItem)">Lekarze</a>
+              <a class="navbar-item" #PersonelNavbarItem
+                 (click)="navigationItemClicked(personel, personelNavbarItem)">Personel</a>
+              <a class="navbar-item" #UrlopyNavbarItem
+                 (click)="navigationItemClicked(urlopy, urlopyNavbarItem)">Urlopy</a>
+              <a class="navbar-item" #PokojeNavbarItem
+                 (click)="navigationItemClicked(pokoje, pokojeNavbarItem)">Pokoje</a>
+              <span class="navbar-slider" [ngStyle]="{'width.px': sliderWidth, 'left.px': sliderLeft}"></span>
+          </div>
       </div>
-    </section>
 
-    <main class="et-main">
-      <section class="et-slide" #Oddziały>
-        <h1>Oddziały</h1>
-      </section>
-      <section class="et-slide" #Pacjenci>
-        <h1>Pacjenci</h1>
-      </section>
-      <section class="et-slide" #Pracownicy>
-        <h1>Pracownicy</h1>
-      </section>
-      <section class="et-slide" #Lekarze>
-        <h1>Lekarze</h1>
-      </section>
-      <section class="et-slide" #Personel>
-        <h1>Personel</h1>
-      </section>
-      <section class="et-slide" #Urlopy>
-        <h1>Urlopy</h1>
-      </section>
-      <section class="et-slide" #Pokoje>
-        <h1>Pokoje</h1>
-      </section>
-    </main>
+      <div class="section" #Oddzialy>
+          <h1>Oddziały</h1>
+      </div>
+      <div class="section" #Pacjenci>
+          <h1>Pacjenci</h1>
+      </div>
+      <div class="section" #Pracownicy>
+          <h1>Pracownicy</h1>
+      </div>
+      <div class="section" #Lekarze>
+          <h1>Lekarze</h1>
+      </div>
+      <div class="section" #Personel>
+          <h1>Personel</h1>
+      </div>
+      <div class="section" #Urlopy>
+          <h1>Urlopy</h1>
+      </div>
+      <div class="section" #Pokoje>
+          <h1>Pokoje</h1>
+      </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  currentId: number;
-  currentTab: ElementRef;
-  tabContainerHeight: number = 70;
-  fixed: boolean = false;
-  sliderWidth: number = 0;
-  sliderLeft: number = 0;
-  pageSections: PageSection[];
-
   @ViewChild('header', {read: ElementRef, static: false}) home: ElementRef;
-  @ViewChild('Oddziały', {read: ElementRef, static: false}) oddzialy: ElementRef;
+  @ViewChild('Oddzialy', {read: ElementRef, static: false}) oddzialy: ElementRef;
   @ViewChild('Pacjenci', {read: ElementRef, static: false}) pacjenci: ElementRef;
   @ViewChild('Pracownicy', {read: ElementRef, static: false}) pracownicy: ElementRef;
   @ViewChild('Lekarze', {read: ElementRef, static: false}) lekarze: ElementRef;
@@ -77,7 +67,7 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('Pokoje', {read: ElementRef, static: false}) pokoje: ElementRef;
 
   @ViewChild('HomeNavbarItem', {read: ElementRef, static: false}) homeNavbarItem: ElementRef;
-  @ViewChild('OddziałyNavbarItem', {read: ElementRef, static: false}) oddzialyNavbarItem: ElementRef;
+  @ViewChild('OddzialyNavbarItem', {read: ElementRef, static: false}) oddzialyNavbarItem: ElementRef;
   @ViewChild('PacjenciNavbarItem', {read: ElementRef, static: false}) pacjenciNavbarItem: ElementRef;
   @ViewChild('PracownicyNavbarItem', {read: ElementRef, static: false}) pracownicyNavbarItem: ElementRef;
   @ViewChild('LekarzeNavbarItem', {read: ElementRef, static: false}) lekarzeNavbarItem: ElementRef;
@@ -85,6 +75,12 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('UrlopyNavbarItem', {read: ElementRef, static: false}) urlopyNavbarItem: ElementRef;
   @ViewChild('PokojeNavbarItem', {read: ElementRef, static: false}) pokojeNavbarItem: ElementRef;
 
+  currentTab: ElementRef = this.homeNavbarItem;
+  tabContainerHeight: number = 70;
+  fixed: boolean = false;
+  sliderWidth: number = 0;
+  sliderLeft: number = 0;
+  pageSections: PageSection[];
 
   ngAfterViewInit(): void {
     this.pageSections = [
@@ -132,9 +128,7 @@ export class AppComponent implements AfterViewInit {
 
   @HostListener("window:resize", [])
   onResize() {
-    if (this.currentId) {
-      this.setSliderCss();
-    }
+    this.setSliderCss();
   }
 
   checkTabContainerPosition() {
