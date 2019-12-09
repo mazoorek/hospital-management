3ea8ac5c2cc4f4a8068aecddfd5a20e0e178c3b6
mazoorek <piotr.mazurek98@gmail.com>
2019-12-09 01:11:30 +0100

    dodanie initializera w springu automatycznie ladujacego dane

--- src/main/java/com/mazamski/hospital/ApplicationContextConfiguration.java
+++ src/main/java/com/mazamski/hospital/ApplicationContextConfiguration.java
@@ -2,10 +2,15 @@ package com.mazamski.hospital;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -25,16 +30,34 @@ public class ApplicationContextConfiguration {
     @Value("${spring.datasource.password}")
     private String dataSourcePassword;
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String dataSourceDriver;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadData()
+    {
+        // do something
+    }
+
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create()
                 .url(dataSourceUrl)
                 .username(dataSourceUserName)
                 .password(dataSourcePassword)
+                .driverClassName(dataSourceDriver)
                 .build();
     }
 
-
+    @Bean
+    public DataSourceInitializer dataSourceInitializer() {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("create_database.sql"));
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource());
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
+    }
 
     @Bean
     public PlatformTransactionManager txManager() {
