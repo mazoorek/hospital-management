fa0773487b06801f95eaaaa529449e20921b86b8
mazoorek <piotr.mazurek98@gmail.com>
2020-01-03 12:52:16 +0100

    dodanie-nowej-nazwy-oddzialu

--- frontend/src/common/List/list.component.ts
+++ frontend/src/common/List/list.component.ts
@@ -1,45 +1,56 @@
 import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 import {ListContent, Row} from "./ListContent/list-content.model";
 
-const MAX_ROW_WIDTH: number = 170;
-
 @Component({
   selector: 'list',
   template: `
-    <div class="list-filter"
-         [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
-      <div class="primary-row-item" *ngFor="let filterValue of filterValues; let i = index;  trackBy:trackByFn">
-        <input class="filter-input"
-               type="text"
-               [(ngModel)]="filterValue"
-               (ngModelChange)="applyFilter(filterValue, i)">
-      </div>
-      <action-button [aquamarine]="true"
-                     (click)="onResetFilter()"
-                     text="zresetuj filtr"></action-button>
-    </div>
-
-    <div class="list-header"
-         [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
-      <div class="primary-row-item"
-           (click)="sortRows(headerColumn)"
-           *ngFor="let headerColumn of headerColumns">
-        {{headerColumn}}
-        <span *ngIf="headerColumn===sortingColumn">&darr;</span>
-      </div>
-      <action-button [transparent]="true"></action-button>
-    </div>
-    <div class="rows"
-         [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
-      <div class="row" *ngFor="let row of filteredRows; let i = index">
-        <div class="row-item" *ngFor="let rowItem of row.row">
-          {{rowItem}}
+    <div class="list-container"
+         [ngStyle]=" {'width':getHeaderColumns().length>1 ? (getMaxRowLength() + 2* LIST_PADDING) + '.px': 'unset'}">
+      <div class="list-filter"
+           [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
+        <div class="primary-row-item" *ngFor="let filterValue of filterValues; let i = index;  trackBy:trackByFn">
+          <input class="filter-input"
+                 type="text"
+                 [(ngModel)]="filterValue"
+                 (ngModelChange)="applyFilter(filterValue, i)">
         </div>
-        <action-button [red]="true"
-                       text="usuń"
-                       (click)="removeRow(i)"
-                       [ngStyle]="{'margin-left.px': 30}"></action-button>
+        <action-button [transparent]="true" [ngStyle]="{'margin-left.px': 30}"></action-button>
+        <action-button [aquamarine]="true"
+                       (click)="onResetFilter()"
+                       text="zresetuj filtr"></action-button>
       </div>
+
+      <div class="list-header"
+           [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
+        <div class="primary-row-item"
+             (click)="sortRows(headerColumn)"
+             *ngFor="let headerColumn of headerColumns">
+          {{headerColumn}}
+          <span *ngIf="headerColumn===sortingColumn">&darr;</span>
+        </div>
+        <action-button [ngStyle]="{'margin-left.px': 30}" [transparent]="true"></action-button>
+        <action-button [transparent]="true"></action-button>
+      </div>
+      <div class="rows"
+           [ngStyle]=" {'width':getHeaderColumns().length>1 ? getMaxRowLength() + '.px': 'unset'}">
+        <div class="row" *ngFor="let row of filteredRows; let i = index">
+          <div class="row-item" tooltipText="{{rowItem}}" *ngFor="let rowItem of row.row">
+            {{rowItem}}
+          </div>
+          <action-button [aquamarine]="true"
+                         text="edytuj"
+                         (click)="onClickAddOrEditRow(i)"
+                         [ngStyle]="{'margin-left.px': 30}"></action-button>
+          <action-button [red]="true"
+                         text="usuń"
+                         (click)="removeRow(i)"></action-button>
+        </div>
+      </div>
+      <action-button class="add-button"
+                     (click)="onClickAddOrEditRow()"
+                     [green]="true"
+                     text="Dodaj nowy rekord"
+                     [width]="200"></action-button>
     </div>
   `,
   styleUrls: ['./list.component.scss']
@@ -47,8 +58,11 @@ const MAX_ROW_WIDTH: number = 170;
 export class ListComponent implements OnInit {
   @Input() listContent: ListContent;
   @Output() removeRowChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() addOrUpdateRowChange: EventEmitter<number> = new EventEmitter<number>();
 
   readonly BUTTON_SPACE: number = 90;
+  readonly MAX_ROW_WIDTH: number = 170;
+  readonly LIST_PADDING: number = 20;
 
   headerColumns: string [];
   filterValues: string [];
@@ -68,12 +82,16 @@ export class ListComponent implements OnInit {
     return index;
   }
 
+  onClickAddOrEditRow(id?: number): void {
+     id ? this.addOrUpdateRowChange.emit(id) : this.addOrUpdateRowChange.emit(-1);
+  }
+
   getHeaderColumns(): string[] {
     return this.listContent.columns.map(column => column.toUpperCase());
   }
 
   getMaxRowLength(): number {
-    return this.getHeaderColumns().length * MAX_ROW_WIDTH + this.BUTTON_SPACE;
+    return this.getHeaderColumns().length * this.MAX_ROW_WIDTH + 2 * this.BUTTON_SPACE;
   }
 
   removeRow(rowIndex: number): void {
@@ -93,11 +111,11 @@ export class ListComponent implements OnInit {
         } else {
           if (b.length > 0 && a.length == 0) {
             return 1;
-          } else if(a.length > 0 && b.length == 0) {
+          } else if (a.length > 0 && b.length == 0) {
             return -1;
           } else if (a > b) {
             return 1;
-          } else if (b > a ) {
+          } else if (b > a) {
             return -1;
           } else {
             return 0;
